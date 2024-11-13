ad806984a6fa656c0bbaa3596e04f6e9bcf04a02
ejmichael <ethanj.michael03@gmail.com>
2024-11-14 01:53:18 +0200

    updated api key in order controller

--- client/src/pages/ViewProduct.jsx
+++ client/src/pages/ViewProduct.jsx
@@ -48,7 +48,7 @@ const ViewProduct = () => {
                     <p className='text-slate-600 my-4'>{productInfo.productDescription}</p>
                     <div className='flex items-end gap-2'>
                         <p className='my-4 text-2xl text-green-600'>R {(productInfo.price).toFixed(2)}</p>
-                        <p className='my-4 text-slate-500 line-through'>R {(productInfo.price/2.21667).toFixed(2)}</p>
+                        <p className='my-4 text-slate-500 line-through'>R {(productInfo.price*2.21667).toFixed(2)}</p>
                     </div>
                     <button 
                         className='my-4 h-[50px] w-[100%] my-3 text-white rounded-full bg-gradient-to-r from-purple-500 to-pink-500'
