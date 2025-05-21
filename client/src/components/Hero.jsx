e8f977a447079d739c46c2795a980ccdc3ff17ff
ejmichael <ethanj.michael03@gmail.com>
2025-05-21 22:46:45 +0200

    first commit to pressure washing landing page

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,32 +7,31 @@ const Hero = () => {
       {/* Background Image */}
       <img 
         className="absolute top-0 left-0 w-full h-full object-cover"
-        src="https://i.postimg.cc/HnhhCdH0/pet-training-bg.png" 
-        alt="Dog mom training her fur baby."
+        src="https://i.postimg.cc/yNSp5vgy/pressure-washing-bg.jpg" 
+        alt="Pressure washing in action"
       />
 
       {/* Overlay */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-40"></div>
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
         <h1 className="text-white md:text-5xl font-bold mb-6">
-          <span className="font-bebas-neue text-7xl ">Train Your Pet Like a Pro</span>
+          <span className="font-bebas-neue text-7xl">Restore Your Home’s Curb Appeal</span>
           <br />
           <span className="font-dancing-script font-light text-4xl lg:text-6xl text-white">
-            Happy dog. Happier home.
+            Fast. Affordable. Professional.
           </span>
         </h1>
 
         <p className="text-gray-100 text-lg md:text-xl max-w-2xl mb-8">
-          Discover step-by-step training guides and behavior tips to help your dog become well-behaved, confident, and happy.
+          Book your local pressure washing experts for driveways, walls, roofs, patios & more. Let your property shine again.
         </p>
 
         <div className="flex space-x-4">
-          <Link to="https://www.braintraining4dogs.com/get-btfd/?&shield=64593qkftzr-h6b867meaasbv1">
-          {/* <Link to="https://www.digistore24.com/redir/434590/ejmichael/"> */}
+          <Link to="/book">
             <button className="bg-[#03989e] text-white font-semibold px-10 py-3 rounded-lg hover:bg-[#027a80] transition">
-              Get the Training Guide Now
+              Get a Free Quote Now
             </button>
           </Link>
         </div>
